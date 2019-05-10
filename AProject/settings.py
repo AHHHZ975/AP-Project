c778d5e37c5d40acf2c2c6977bbfb83a0e28496e
Amir Hossein Zamani <ahz.975@yahoo.com>
2019-05-10 02:41:42 -0400

    Render an HTML file.

--- AProject/settings.py
+++ AProject/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'AProject/templates/bourse')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bourse.apps.BourseConfig',
+    'bourse',
     'mysite'
 
 ]
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'AProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [TEMPLATE_DIR]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
